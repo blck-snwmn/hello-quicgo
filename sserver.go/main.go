c6a88ad4034860c402d5a47f841d728d160b8520
blck-snwmn <whitesnowmancreator@gmail.com>
2023-04-04 02:26:18 +0900

    Refactor

--- sserver.go/main.go
+++ sserver.go/main.go
@@ -7,11 +7,20 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"io"
 	"log"
 	"math/big"
 
 	"github.com/quic-go/quic-go"
+)
+
+var (
+	addr    = ":4433"
+	tlsConf = &tls.Config{
+		Certificates: []tls.Certificate{tlsCert()},
+		NextProtos:   []string{"quic-echo-example"},
+	}
 )
 
 func main() {
@@ -23,27 +32,36 @@ func main() {
 
 func s() error {
 	addr := ":4433"
-	c := &tls.Config{
-		Certificates: []tls.Certificate{tlsCert()},
-		NextProtos:   []string{"quic-echo-example"},
-	}
-	listener, err := quic.ListenAddr(addr, c, nil)
+	listener, err := quic.ListenAddr(addr, tlsConf, nil)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to listen: %v", err)
 	}
-	conn, err := listener.Accept(context.Background())
-	if err != nil {
-		return err
+	for {
+		conn, err := listener.Accept(context.Background())
+		if err != nil {
+			return fmt.Errorf("failed to accept: %v", err)
+		}
+		go func() {
+			fmt.Println("recieved connection")
+			stream, err := conn.AcceptStream(context.Background())
+			if err != nil {
+				panic(err)
+			}
+			_, err = io.Copy(loggingWriter{stream}, stream)
+			if err != nil {
+				log.Printf("failed to copy: %v\n", err)
+				return
+			}
+		}()
 	}
-	stream, err := conn.AcceptStream(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	// Echo through the loggingWriter
-	_, err = io.Copy(stream, stream)
-	return err
 }
 
+type loggingWriter struct{ io.Writer }
+
+func (w loggingWriter) Write(b []byte) (int, error) {
+	fmt.Printf("Server: Got '%s'\n", string(b))
+	return w.Writer.Write(b)
+}
 
 func tlsCert() tls.Certificate {
 	key, _ := rsa.GenerateKey(rand.Reader, 1024)
