09c9a5d51b185a380b5fd1d171f5520ab6fa67ba
blck-snwmn <whitesnowmancreator@gmail.com>
2023-04-13 23:57:20 +0900

    Extract function

--- sserver/main.go
+++ sserver/main.go
@@ -43,56 +43,58 @@ func startServer() error {
 		if err != nil {
 			return fmt.Errorf("failed to accept: %v", err)
 		}
-		go func() {
-			fmt.Println("recieved connection")
-			defer fmt.Println("close connection")
-			stream, err := conn.AcceptStream(context.Background())
-			if err != nil {
-				panic(err)
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn quic.Connection) {
+	fmt.Println("recieved connection")
+	defer fmt.Println("close connection")
+	stream, err := conn.AcceptStream(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		fmt.Printf("streamID=%d\n", stream.StreamID())
+
+		var lengthBuf [4]byte
+		_, err := stream.Read(lengthBuf[:])
+		if err != nil {
+			if err == io.EOF {
+				return
 			}
+			fmt.Printf("failed to read length: %v\n", err)
+			return
+		}
+		length := binary.LittleEndian.Uint32(lengthBuf[:])
+		fmt.Printf("length=%d\n", length)
 
-			for {
-				fmt.Printf("streamID=%d\n", stream.StreamID())
-
-				var lengthBuf [4]byte
-				_, err := stream.Read(lengthBuf[:])
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					fmt.Printf("failed to read length: %v\n", err)
-					return
-				}
-				length := binary.LittleEndian.Uint32(lengthBuf[:])
-				fmt.Printf("length=%d\n", length)
-
-				buf := make([]byte, length)
-				_, err = stream.Read(buf)
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					fmt.Printf("failed to read message: %v\n", err)
-					return
-				}
-
-				user := fbs.GetRootAsUser(buf, 0)
-
-				var pos fbs.Position
-				user.Pos(&pos)
-
-				fmt.Printf("name=`%s`, ", user.Name())
-				fmt.Printf("color=%v, ", user.Color())
-				fmt.Printf("position{x, y, z} = {%v, %v, %v}, ", pos.X(), pos.Y(), pos.Z())
-
-				for i := 0; i < user.InventoryLength(); i++ {
-					var item fbs.Item
-					user.Inventory(&item, i)
-					fmt.Printf("item[%d]:name=%s,", i, item.Name())
-				}
-				fmt.Println()
+		buf := make([]byte, length)
+		_, err = stream.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return
 			}
-		}()
+			fmt.Printf("failed to read message: %v\n", err)
+			return
+		}
+
+		user := fbs.GetRootAsUser(buf, 0)
+
+		var pos fbs.Position
+		user.Pos(&pos)
+
+		fmt.Printf("name=`%s`, ", user.Name())
+		fmt.Printf("color=%v, ", user.Color())
+		fmt.Printf("position{x, y, z} = {%v, %v, %v}, ", pos.X(), pos.Y(), pos.Z())
+
+		for i := 0; i < user.InventoryLength(); i++ {
+			var item fbs.Item
+			user.Inventory(&item, i)
+			fmt.Printf("item[%d]:name=%s,", i, item.Name())
+		}
+		fmt.Println()
 	}
 }
 
